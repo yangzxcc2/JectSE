63e315583ae33bef12c4e6bf6a31384e53b3220a
Thanawat Lukuan <thanawat_lukuan@cmu.ac.th>
2017-03-13 17:28:32 +0700

    Create the stub file for the currency

---
+++ src/utility/Currency.java
@@ -0,0 +1,11 @@
+package utility;
+
+/**
+ * Created by macintoshhd on 3/13/2017 AD.
+ */
+public class Currency {
+    public double exchange(String sourceCurrency, double amount,String targetCurrency){
+
+        return 0.0;
+    }
+}
