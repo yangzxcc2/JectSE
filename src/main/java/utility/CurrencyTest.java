f277b85082415bf830d19f657801adbf2a569517
kiky <panita_tip@cmu.ac.th>
2017-03-13 18:18:37 +0700

    create test cases by kiky

--- src/main/java/utility/CurrencyTest.java
+++ src/main/java/utility/CurrencyTest.java
@@ -13,5 +13,6 @@ public class CurrencyTest {
     public void testExchange(){
         Currency currency= new Currency();
         assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1.0,0.001));
+        assertThat(currency.exchange("USD",1.0,"THB"),closeTo(1.0,0.001));
     }
 }
