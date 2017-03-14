c837404841856fe9401d2448e5d686d3ddde056f
Siriganya Sensupa <siriganya_sen@cmu.ac.th>
2017-03-14 15:00:02 +0700

    exchange method

--- src/main/java/utility/Currency.java
+++ src/main/java/utility/Currency.java
@@ -4,8 +4,48 @@ package utility;
  * Created by macintoshhd on 3/13/2017 AD.
  */
 public class Currency {
-    public double exchange(String sourceCurrency, double amount,String targetCurrency){
+    public double exchange(String sourceCurrency, double amount,String targetCurrency) {
 
-        return 0.0;
+        if(sourceCurrency == targetCurrency){
+            return amount;
+        }else if (sourceCurrency == "USD"){
+            return sourceIsUSD(sourceCurrency,amount,targetCurrency);
+        }else if (targetCurrency == "USD"){
+            return targetIsUSD(sourceCurrency,amount,targetCurrency);
+        }else{
+            return bothIsNotUSD(sourceCurrency,amount,targetCurrency);
+        }
+    }
+
+    private double targetIsUSD (String sourceCurrency, double amount, String targetCurrency){
+        if(sourceCurrency == "THB" ){
+            amount = amount/33.0;
+            return amount;
+        }else{
+            amount = amount / 1.01;
+            return amount;
+        }
+    }
+
+    private double bothIsNotUSD (String sourceCurrency, double amount, String targetCurrency) {
+        if (sourceCurrency == "THB") {
+            amount = amount / 33.0;
+            amount = amount * 1.01;
+            return amount;
+        } else {
+            amount = amount / 1.01;
+            amount = amount * 33;
+            return amount;
+        }
+    }
+
+    private double sourceIsUSD (String sourceCurrency, double amount, String targetCurrency){
+        if( targetCurrency == "THB"){
+            amount = amount * 33.0;
+            return amount;
+        } else {
+            amount = amount * 1.01;
+            return amount;
+        }
     }
 }
