aca54a65d82838fde8f05fe5ebb749953cc2858f
Thanawat Lukuan <thanawat_lukuan@cmu.ac.th>
2017-03-14 15:17:14 +0700

    Done!

--- src/main/java/utility/CurrencyTest.java
+++ src/main/java/utility/CurrencyTest.java
@@ -13,9 +13,10 @@ public class CurrencyTest {
     public void testExchange(){
         Currency currency= new Currency();
         assertThat(currency.exchange("USD",1.0,"USD"),closeTo(1.0,0.001));
-        assertThat(currency.exchange("USD",1.0,"THB"),closeTo(35.0,0.001));
+        assertThat(currency.exchange("USD",1.0,"THB"),closeTo(33.0,0.001));
         assertThat(currency.exchange("USD",1.0,"EU"),closeTo(1.01,0.001));
-        assertThat(currency.exchange("EU",1.0,"THB"),closeTo(33.33,0.001));
+        assertThat(currency.exchange("EU",1.0,"USD"),closeTo(0.99,0.001));
+
 
     }
 }
